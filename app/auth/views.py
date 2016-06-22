c57dac4a7ade89ad1d527f7b70fe0b90609340cc
Hannah Masila <hannahmasila@gmail.com>
2016-06-23 00:03:31 +0300

    Authentication forms and views

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,19 +1,9 @@
-from flask import render_template, redirect, request, url_for, flash, g, session
-from flask_login import login_user, logout_user, login_required
+from flask import render_template, redirect, request, url_for, flash, session
+from flask_login import logout_user, login_required
 from . import auth
 from .. import db
 from ..models import Users
 from .forms import LoginForm, RegistrationForm
-
-
-@auth.before_request
-def before_request():
-    """
-      pull user's profile from the database before every request are treated
-    """
-    g.user = None
-    if 'user_id' in session:
-        g.user = Users.query.get(session['user_id'])
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -24,8 +14,9 @@ def login():
         # user should be an instance of your `Users` class
         user = Users.query.filter_by(username=form.username.data).first()
         if user is not None and user.verify_password(form.password.data):
+            user.authenticated = True
             session['user_id'] = user.id
-            flash('Welcome %s' % user.name)
+            flash('Welcome %s' % user.username)
         # next_is_valid should check if the user has valid
         # permission to access the `next` url
             return redirect(request.args.get('next') or url_for('main.home'))
@@ -36,7 +27,7 @@ def login():
 
 
 @auth.route('/logout')
-@login_required
+# @login_required
 def logout():
     logout_user()
     flash('You have been logged out.')
@@ -50,15 +41,15 @@ def signup():
     if form.validate_on_submit():
         user = Users(form.first_name.data,
                      form.last_name.data,
-                     form.phone.data,
                      form.username.data,
-                     form.password.data
+                     form.password.data,
+                     form.phone.data
                      )
         db.session.add(user)
         db.session.commit()
         # Log the user in, as he now has an id
         session['user_id'] = user.id
-        flash('Welcome to Online Store!')
+        flash('Welcome %s' % user.username)
         # after first signup, you will be redirected to the login page
         return redirect(url_for('main.home'))
         # Render the signup.html from the templates folder
