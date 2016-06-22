db9f282a7176f7478cf3bedb44b1db1011b1ba84
Hannah Masila <hannahmasila@gmail.com>
2016-06-22 11:00:56 +0300

    This file contains the migrations to the db and also the main app.run

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from app.models import Users, Products, Store
-from flask.ext.script import Manager, Shell
-from flask.ext.migrate import Migrate, MigrateCommand
+from app.models import Users, Products, Stores
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
@@ -15,13 +15,17 @@ def make_shell_context():
     to the shell importing them automatically
     on `python manager.py shell`.
     '''
+    # db is database
+    # Users is instance of the users table
+    # Products is instance of products table
+    # Stores is instance of stores table
     return dict(app=app, db=db, Users=Users, Products=Products,
-                Store=Store)
+                Stores=Stores)
 
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+    # app.run()
